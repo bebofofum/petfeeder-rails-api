7dc62afbe370431beec573fe55d165638370008a
bebofofum <bri.boehm@gmail.com>
2021-03-23 17:31:14 -0400

    Added validations to owner and pet models.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,5 +2,7 @@ class Owner < ApplicationRecord
     has_many :feedings, dependent: :destroy
     has_many :pets, through: :feedings
 
+    validates :name, presence: true, uniqueness: true
+
 
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,5 +2,8 @@ class Pet < ApplicationRecord
     has_many :feedings
     has_many :owners, through: :feedings
 
+    validates :name, presence: true, uniqueness: true
+    validates :species, presence: true
+
 
 end
