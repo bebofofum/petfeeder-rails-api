f15d2b4fdc75ca12e9b6f0d3b0d532f03a8753dd
bebofofum <bri.boehm@gmail.com>
2021-06-06 14:41:20 -0400

    Added necessary include Devise::JWT:RevocationStrategies::JTIMatcher to top of User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   has_many :pets
   has_many :owners
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
   
 end
